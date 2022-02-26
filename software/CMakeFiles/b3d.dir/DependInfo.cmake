ccd1ea76e1f0dcc85b1233db925a1e62a64bef42
scottedwardpratt <prattsc@msu.edu>
2022-02-26 10:05:38 -0500

    need to fix this crap

--- software/CMakeFiles/b3d.dir/DependInfo.cmake
+++ software/CMakeFiles/b3d.dir/DependInfo.cmake
@@ -1,45 +1,51 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/scott/git/b3d/software/src/acceptance.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/acceptance.cc.o"
+  "/home/scott/git/b3d/software/src/acceptance_ALICE.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/acceptance_ALICE.cc.o"
+  "/home/scott/git/b3d/software/src/acceptance_STAR.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/acceptance_STAR.cc.o"
+  "/home/scott/git/b3d/software/src/acceptance_cheap.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/acceptance_cheap.cc.o"
+  "/home/scott/git/b3d/software/src/action.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/action.cc.o"
+  "/home/scott/git/b3d/software/src/action_perform.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/action_perform.cc.o"
+  "/home/scott/git/b3d/software/src/action_perform_activate.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/action_perform_activate.cc.o"
+  "/home/scott/git/b3d/software/src/action_perform_collide.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/action_perform_collide.cc.o"
+  "/home/scott/git/b3d/software/src/action_perform_decay.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/action_perform_decay.cc.o"
+  "/home/scott/git/b3d/software/src/action_perform_exitcell.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/action_perform_exitcell.cc.o"
+  "/home/scott/git/b3d/software/src/action_perform_mutcalc.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/action_perform_mutcalc.cc.o"
+  "/home/scott/git/b3d/software/src/addaction.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/addaction.cc.o"
+  "/home/scott/git/b3d/software/src/annihilate.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/annihilate.cc.o"
+  "/home/scott/git/b3d/software/src/b3d.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/b3d.cc.o"
+  "/home/scott/git/b3d/software/src/b3dio.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/b3dio.cc.o"
+  "/home/scott/git/b3d/software/src/b3dsubs.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/b3dsubs.cc.o"
+  "/home/scott/git/b3d/software/src/balancearrays.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/balancearrays.cc.o"
+  "/home/scott/git/b3d/software/src/bfnumer.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/bfnumer.cc.o"
+  "/home/scott/git/b3d/software/src/cell.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/cell.cc.o"
+  "/home/scott/git/b3d/software/src/collide.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/collide.cc.o"
+  "/home/scott/git/b3d/software/src/decay.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/decay.cc.o"
+  "/home/scott/git/b3d/software/src/findcollision.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/findcollision.cc.o"
+  "/home/scott/git/b3d/software/src/hyper.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/hyper.cc.o"
+  "/home/scott/git/b3d/software/src/inelastic.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/inelastic.cc.o"
+  "/home/scott/git/b3d/software/src/makepart.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/makepart.cc.o"
+  "/home/scott/git/b3d/software/src/mutinfo.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/mutinfo.cc.o"
+  "/home/scott/git/b3d/software/src/part.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/part.cc.o"
+  "/home/scott/git/b3d/software/src/resinfo.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/resinfo.cc.o"
+  "/home/scott/git/b3d/software/src/reslist.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/reslist.cc.o"
+  "/home/scott/git/b3d/software/src/sampler.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/sampler.cc.o"
+  "/home/scott/git/b3d/software/src/scatter.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/scatter.cc.o"
+  "/home/scott/git/b3d/software/src/seinfo.cc" "/home/scott/git/b3d/software/CMakeFiles/b3d.dir/src/seinfo.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/scottpratt/git/b3d/software/src/acceptance.cc" "CMakeFiles/b3d.dir/src/acceptance.cc.o" "gcc" "CMakeFiles/b3d.dir/src/acceptance.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/acceptance_ALICE.cc" "CMakeFiles/b3d.dir/src/acceptance_ALICE.cc.o" "gcc" "CMakeFiles/b3d.dir/src/acceptance_ALICE.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/acceptance_STAR.cc" "CMakeFiles/b3d.dir/src/acceptance_STAR.cc.o" "gcc" "CMakeFiles/b3d.dir/src/acceptance_STAR.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/acceptance_cheap.cc" "CMakeFiles/b3d.dir/src/acceptance_cheap.cc.o" "gcc" "CMakeFiles/b3d.dir/src/acceptance_cheap.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/action.cc" "CMakeFiles/b3d.dir/src/action.cc.o" "gcc" "CMakeFiles/b3d.dir/src/action.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/action_perform.cc" "CMakeFiles/b3d.dir/src/action_perform.cc.o" "gcc" "CMakeFiles/b3d.dir/src/action_perform.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/action_perform_activate.cc" "CMakeFiles/b3d.dir/src/action_perform_activate.cc.o" "gcc" "CMakeFiles/b3d.dir/src/action_perform_activate.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/action_perform_collide.cc" "CMakeFiles/b3d.dir/src/action_perform_collide.cc.o" "gcc" "CMakeFiles/b3d.dir/src/action_perform_collide.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/action_perform_decay.cc" "CMakeFiles/b3d.dir/src/action_perform_decay.cc.o" "gcc" "CMakeFiles/b3d.dir/src/action_perform_decay.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/action_perform_exitcell.cc" "CMakeFiles/b3d.dir/src/action_perform_exitcell.cc.o" "gcc" "CMakeFiles/b3d.dir/src/action_perform_exitcell.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/action_perform_mutcalc.cc" "CMakeFiles/b3d.dir/src/action_perform_mutcalc.cc.o" "gcc" "CMakeFiles/b3d.dir/src/action_perform_mutcalc.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/addaction.cc" "CMakeFiles/b3d.dir/src/addaction.cc.o" "gcc" "CMakeFiles/b3d.dir/src/addaction.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/annihilate.cc" "CMakeFiles/b3d.dir/src/annihilate.cc.o" "gcc" "CMakeFiles/b3d.dir/src/annihilate.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/b3d.cc" "CMakeFiles/b3d.dir/src/b3d.cc.o" "gcc" "CMakeFiles/b3d.dir/src/b3d.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/b3dio.cc" "CMakeFiles/b3d.dir/src/b3dio.cc.o" "gcc" "CMakeFiles/b3d.dir/src/b3dio.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/b3dsubs.cc" "CMakeFiles/b3d.dir/src/b3dsubs.cc.o" "gcc" "CMakeFiles/b3d.dir/src/b3dsubs.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/balancearrays.cc" "CMakeFiles/b3d.dir/src/balancearrays.cc.o" "gcc" "CMakeFiles/b3d.dir/src/balancearrays.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/bfnumer.cc" "CMakeFiles/b3d.dir/src/bfnumer.cc.o" "gcc" "CMakeFiles/b3d.dir/src/bfnumer.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/cell.cc" "CMakeFiles/b3d.dir/src/cell.cc.o" "gcc" "CMakeFiles/b3d.dir/src/cell.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/collide.cc" "CMakeFiles/b3d.dir/src/collide.cc.o" "gcc" "CMakeFiles/b3d.dir/src/collide.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/decay.cc" "CMakeFiles/b3d.dir/src/decay.cc.o" "gcc" "CMakeFiles/b3d.dir/src/decay.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/findcollision.cc" "CMakeFiles/b3d.dir/src/findcollision.cc.o" "gcc" "CMakeFiles/b3d.dir/src/findcollision.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/hyper.cc" "CMakeFiles/b3d.dir/src/hyper.cc.o" "gcc" "CMakeFiles/b3d.dir/src/hyper.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/inelastic.cc" "CMakeFiles/b3d.dir/src/inelastic.cc.o" "gcc" "CMakeFiles/b3d.dir/src/inelastic.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/makepart.cc" "CMakeFiles/b3d.dir/src/makepart.cc.o" "gcc" "CMakeFiles/b3d.dir/src/makepart.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/mutinfo.cc" "CMakeFiles/b3d.dir/src/mutinfo.cc.o" "gcc" "CMakeFiles/b3d.dir/src/mutinfo.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/part.cc" "CMakeFiles/b3d.dir/src/part.cc.o" "gcc" "CMakeFiles/b3d.dir/src/part.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/resinfo.cc" "CMakeFiles/b3d.dir/src/resinfo.cc.o" "gcc" "CMakeFiles/b3d.dir/src/resinfo.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/reslist.cc" "CMakeFiles/b3d.dir/src/reslist.cc.o" "gcc" "CMakeFiles/b3d.dir/src/reslist.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/sampler.cc" "CMakeFiles/b3d.dir/src/sampler.cc.o" "gcc" "CMakeFiles/b3d.dir/src/sampler.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/scatter.cc" "CMakeFiles/b3d.dir/src/scatter.cc.o" "gcc" "CMakeFiles/b3d.dir/src/scatter.cc.o.d"
-  "/Users/scottpratt/git/b3d/software/src/seinfo.cc" "CMakeFiles/b3d.dir/src/seinfo.cc.o" "gcc" "CMakeFiles/b3d.dir/src/seinfo.cc.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "/home/scott/git/commonutils/software/include"
+  "/home/scott/git/coral/software/include"
+  "../hydrobalance/include"
+  "/usr/include/eigen3"
   )
 
 # Targets to which this target links.
